3ea87ff9eb4dbebcea8b429030e5ca2e2cf47e34
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-27 22:03:47 -0400

    edited orm.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3000,
+  port: 3306,
   user: "root",
   password: "Dlekwjd347",
   database: "burgers_db"
