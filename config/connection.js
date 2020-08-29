f564d74b312ed1f77b91cd9d3f57bd4474155a76
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-29 18:09:17 -0400

    re-deploy with jaws info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Dlekwjd347",
-  database: "burgers_db"
-});
+var connection = null;
+if(typeof process.env.JAWSDB_URL === 'undefined'){
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Dlekwjd347",
+    database: "burgers_db"
+  })
+};
 
 // Make connection.
 connection.connect(function(err) {
