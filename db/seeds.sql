7c310b687859c3416f217d221cb84a4c592cd350
Elizabeth Lee <dlekwjd347@gmail.com>
2020-08-29 20:12:36 -0400

    schema, seeds edits, changed from local host to heroku on mysql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO burgers (burger_name) VALUES ('cheese');
-INSERT INTO burgers (burger_name) VALUES ('italian');
-INSERT INTO burgers (burger_name, devoured) VALUES ('avocado', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('sushi', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('beacon', true);
-INSERT INTO burgers (burger_name) VALUES ('tripple deck');
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('italian');
+INSERT INTO burgers (name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (name, devoured) VALUES ('sushi', true);
+
